99fb7fa411bf2cf359feb3603397b937ac5b267e
Enrique Pittaluga <ep7m@me.com>
2020-03-11 01:09:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,10 @@
-def reverse_each_word()
-  
-  
-  
-  
+def reverse_each_word(sentence)
+  sent_array = sentence.split(" ")
+
+  reverse_array = sent_array.collect do |word|
+    word.reverse
+  end
+
+  puts reverse_array.join(" ")
+
 end
